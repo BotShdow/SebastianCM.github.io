645f63a8ff00ae1d42e9d52e50cc5fac3284e0f9
BotShdow <sebas16016@gmail.com>
2025-05-21 23:26:13 +0200

    Delete javascript directory

--- javascript/CambioTema.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("CambioTema").addEventListener("click", function () {
-    document.body.classList.toggle("TemaOscuro") // Cambia el ícono del botón // 
-    if (document.body.classList.contains("TemaOscuro")) {
-        this.textContent = "🌙"; // Luna 
-    } else {
-        this.textContent = "☀️"; // Sol
-    }
-});
